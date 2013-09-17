bfaad6d5a603e256faaa2d016258dabcda3916be
dbc-apprentice <apprentice@devbootcamp.com>
2013-09-17 16:54:26 -0500

    added category and post lists, post view

--- db/migrate/20130917154406_create_posts.rb
+++ db/migrate/20130917154406_create_posts.rb
@@ -1,4 +1,13 @@
 class CreatePosts < ActiveRecord::Migration
   def change
+    create_table :posts do |t|
+      t.string :title
+      t.text :content
+      t.string :price
+      t.string :email
+      t.string :edit_url
+      t.integer :category_id
+      t.timestamps
+    end
   end
 end
